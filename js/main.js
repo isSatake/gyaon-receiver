ca3b2f60a2148c2e5dbea3ed984a9138dbf69429
stkay <stkayy@gmail.com>
2018-06-17 18:23:15 +0900

    受信先を変更できる

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-var socket = io.connect('http://linda-server.herokuapp.com:80');
-var linda = new Linda().connect(socket);
-
-var ts = linda.tuplespace('masuilab');
-
-linda.io.on('connect', function(){
-  print('socket.io connect!!');
-
-  ts.watch({type:"gyaon"}, function(err, tuple){
-    if(err) return;
-    print(tuple.data);
-    play(tuple.data.url)
-  });
-
-});
-
 var play = function(url){
   var audio = new Audio(url);
   console.log(audio.src)
@@ -38,3 +22,27 @@ var print = function(data){
   var a = `<a href="${url}">${message}</a>`
   $("#container").prepend(`<li>${getTime()} ${user}「${a}」</li>`)
 };
+
+window.onload = function(){
+  var socket = io.connect('http://linda-server.herokuapp.com:80');
+  var source = localStorage.getItem("source") || "global";
+  var linda = new Linda().connect(socket);
+  var ts = linda.tuplespace(source);
+
+  linda.io.on('connect', function(){
+    console.log('socket.io connect!!');
+    watchId = ts.watch({type:"gyaon"}, function(err, tuple){
+      if(err) return;
+      print(tuple.data);
+      play(tuple.data.url)
+    });
+  });
+
+  var setSource = document.getElementById("setSource");
+  setSource.value = source;
+  setSource.onchange = function(event){
+    var source = event.target.value
+    localStorage.setItem("source", source)
+    location.reload()
+  }
+}
